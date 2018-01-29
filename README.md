eff978d910ef79677594af7a8a266de67d103f46
aficustree <shaun@sjbaker.us>
2018-01-29 07:31:59 -0600

    uploading

---
+++ README.md
@@ -0,0 +1,38 @@
+# homebridge-gatestrike
+
+This plugin controls an electric strike interfaced with a Raspberry Pi via a GPIO-controlled relay module. 
+
+## Installation
+
+1. Install homebridge using: `npm install -g homebridge`
+2. Install homebridge-sss-platform using: `npm install -g git+https://github.com/aficustree/homebridge-gatekeeperr`
+3. Update your configuration file. See [sample-config.json](./sample-config.json) in this repository for a sample. 
+
+## Configuration
+
+Please see the `sample-config.json` for details on how to configure. 
+
+```
+"accessories": [
+    {
+        "accessory": "homebridge-gatestrike",
+        "name": "Front Gate",
+        "unlockduration": 10,
+        "unlockurl": "http://YOURIP:YOURPORT/YOURPATH",
+        "listenport": 8900
+    }
+]
+```
+
+1. *accessory* - must be homebridge-gatestrike
+2. *name* - whatever you want to name your accessory
+3. *unlockduration* - how long does your gatestrike remain unlocked on a trigger
+4. *unlockurl* - assumes an HTTP GET to a URL to trigger the unlock event
+5. *listenport* - by default, the system will listen for connections on the defined port. this allows the system to register a trigger that happens outside of homekit (i.e., someone pushes a door release button)
+
+## License
+
+Copyright 2018, [aficustree](https://github.com/aficustree)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the [License](./LICENSE).
+
---
+++ index.js
@@ -0,0 +1,99 @@
+var Service, Characteristic;
+var axios = require('axios');
+
+module.exports = function(homebridge){
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('homebridge-gatestrike', 'gatestrike', GatestrikeAccessory);
+};
+
+class GatestrikeAccessory {
+    constructor(log, config) {
+        this.log = log;
+        this.name = config['name'];
+        this.unlockurl = config['unlockurl'];
+        this.unlockduration = config['unlockduration'];
+        this.port = config['listenport'];
+        
+        this.state = Characteristic.LockCurrentState.SECURED; //default is secured on an electric strike
+
+        this.axiosHeaderConfig = {headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        }};
+
+        this.lockService = new Service.LockMechanism(this.name);
+        this.lockService
+            .getCharacteristic(Characteristic.LockCurrentState)
+            .on('get', (callback)=>this.getState(callback));
+
+        this.lockService
+            .getCharacteristic(Characteristic.LockTargetState)
+            .on('get', (callback)=>this.getState(callback))
+            .on('set', (state, callback)=>this.setState(state, callback));
+
+        try {
+            this.listener = require('http').createServer((req, res)=>this.httpListener(req, res));
+            this.listener.listen(this.port);
+            this.log('listening on port '+this.port);
+        }
+        catch (err) {
+            this.log(err);
+        }
+
+    }
+    
+    httpListener(req, res) {
+        var data = '';
+		
+        if (req.method == 'POST') {
+            req.on('data', (chunk) => {
+                data += chunk;
+            });		
+            req.on('end', () => {
+                this.log('Received notification and body data:');
+                this.log(data.toString());
+            });
+        }	
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end();
+        this.log('pushing that an unlock event was received');
+        this.setState(Characteristic.LockCurrentState.UNSECURED,null);
+    }
+
+    getState(callback) {
+        callback(null, this.state);
+    }
+
+    async setState(state, callback) {
+        if(state!=this.state) {
+            try {
+                this.log('setting state by '+this.unlockurl);
+                var response = await axios.get(this.unlockurl,this.axiosHeaderConfig);
+                this.log(response.data);
+                if(response.status == 200 || response.status == 204) {
+                    this.state=Characteristic.LockCurrentState.UNSECURED;
+                    this.lockService.setCharacteristic(Characteristic.LockCurrentState, this.state);
+                    setTimeout(()=>{
+                        this.log('timeout expired, relocking');
+                        this.state=Characteristic.LockCurrentState.SECURED;
+                        this.lockService.setCharacteristic(Characteristic.LockCurrentState, this.state);
+                    },1000*this.unlockduration); //will return to locked status after duration expires
+                    callback(null,response.data,this.state);
+                }
+                else
+                    throw(response.statusCode);
+            }
+            catch (err) {
+                this.log('error communicating with lock '+err);
+                this.state=Characteristic.LockCurrentState.UNKNOWN;
+                callback(err);
+            }
+        }
+    }
+
+    getServices() {
+        return [this.lockService];
+    }
+
+}
